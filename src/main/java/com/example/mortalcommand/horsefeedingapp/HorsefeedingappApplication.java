8c7a4731583e4cf55ec80a1480f04d67ce7afeeb
Giannikos <david.giannikos@accenture.com>
2023-10-09 18:52:33 +0200

    project initialized

---
+++ src/main/java/com/example/mortalcommand/horsefeedingapp/HorsefeedingappApplication.java
@@ -0,0 +1,13 @@
+package com.example.mortalcommand.horsefeedingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorsefeedingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorsefeedingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mortalcommand/horsefeedingapp/HorsefeedingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mortalcommand.horsefeedingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorsefeedingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
