95dbc4861563d782d703dfb1ee5f6c82175aec27
Giannikos <david.giannikos@accenture.com>
2023-10-15 20:15:12 +0200

    doc comment for exception class added

--- src/main/java/com/example/mortalcommand/horsefeedingapp/exception/FeedingScheduleValidationException.java
+++ src/main/java/com/example/mortalcommand/horsefeedingapp/exception/FeedingScheduleValidationException.java
@@ -1,6 +1,14 @@
 package com.example.mortalcommand.horsefeedingapp.exception;
 
+/**
+ * Customized exception that is thrown when an unvalid feeding schedule is tried to be created.
+ */
 public class FeedingScheduleValidationException extends RuntimeException{
+
+    /**
+     * Constructor that constructs the exception.
+     * @param errorMessage error message that can be provided to the constructor
+     */
     public FeedingScheduleValidationException(String errorMessage) {
         super(errorMessage);
     }
